de206726cfa36917ab0c69b2112f24dad7fee6a5
JuiPharm <jui_3065@hotmail.com>
2026-05-04 17:43:56 +0700

    Update API_URL to point to the correct endpoint

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.INVENTORY_CONFIG = {
   MODE: 'gas',
-  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwz6YOLJmxuy8-8Pqg6B35wQfiyKPNiCqvpdbekCZVg_xkJXEIQScsX_-O8jBg43h2h/exec',
   APP_NAME: 'BHH Rx Inventory',
   HOSPITAL_NAME: 'Bangkok Hospital Hatyai',
   LOGO_URL: '',
